040ab0b462c09a52e387fdc591791308060530b6
pjnewt <pjnewt58@gmail.com>
2026-03-26 11:18:59 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'squat-tribe-v1-3';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.v1.3.css',
+  './app.v1.3.js',
+  './manifest.json',
+  './back.png',
+  './bulgarian.png',
+  './front.png',
+  './side_step.png',
+  './sumo.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(r => r || fetch(req)));
+});
